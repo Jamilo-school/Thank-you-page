245b8e30697c9cdb9f087c81a346d1f8fd4d5c4d
Jamilo-school <geofreyonyango167@gmail.com>
2023-10-14 03:52:46 +0300

    IWW

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function() {
       popupMessage.style.display = "none";
       // Redirect to the new link address after 4 seconds (adjust the time as needed)
       window.location.href = "https://www.google.com/"; // Replace with your desired link address
-    }, 20000); // Adjust the time as needed (3 seconds in this example)
+    }, 120000); // Adjust the time as needed (3 seconds in this example)
   }
 
   // Show the popup message and redirect when the page loads
